f52b8cc99761aa1c73cb082b65d8c88d6ac3c354
hakdag <hakan.akdag@aurea.com>
2019-07-14 13:15:24 +0300

    Updated global asax file.

--- Generator/WebApiGenerator/Templates/Partials/GlobalAsaxTemplate.cs
+++ Generator/WebApiGenerator/Templates/Partials/GlobalAsaxTemplate.cs
@@ -1,20 +1,16 @@
-﻿using System;
+﻿using GeneratorBase;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GeneratorBase;
 
 namespace WebApiGenerator.Templates
 {
     public partial class GlobalAsaxTemplate
     {
-        private List<Module> modules;
+        public List<Module> Modules { get; }
         public string ProjectName { get; }
 
         public GlobalAsaxTemplate(string projectName, List<Module> modules)
         {
-            this.modules = modules;
+            this.Modules = modules;
             ProjectName = projectName;
         }
     }
